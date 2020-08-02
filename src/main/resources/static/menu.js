04add4c332384b9e82b25d366d253a2a08a081e5
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2020-08-02 18:26:41 -0400

    added key-code indicators in UI

--- src/main/resources/static/menu.js
+++ src/main/resources/static/menu.js
@@ -51,23 +51,23 @@ async function showFillState() {
 
     const fillState = await getData('/polyglot/api/');
 
-    $('#niveau0').text("Niveau 1  (" + fillState[0] + ")");
+    $('#niveau0').text("Niveau [1]  (" + fillState[0] + ")");
     if (fillState[0] == 0) {
         $('#niveau0').addClass("disabled");
     }
-    $('#niveau1').text("Niveau 2  (" + fillState[1] + ")");
+    $('#niveau1').text("Niveau [2]  (" + fillState[1] + ")");
     if (fillState[1] == 0) {
         $('#niveau1').addClass("disabled");
     }
-    $('#niveau2').text("Niveau 3  (" + fillState[2] + ")");
+    $('#niveau2').text("Niveau [3]  (" + fillState[2] + ")");
     if (fillState[2] == 0) {
         $('#niveau2').addClass("disabled");
     }
-    $('#niveau3').text("Niveau 4  (" + fillState[3] + ")");
+    $('#niveau3').text("Niveau [4]  (" + fillState[3] + ")");
     if (fillState[3] == 0) {
         $('#niveau3').addClass("disabled");
     }
-    $('#niveau4').text("Niveau 5  (" + fillState[4] + ")");
+    $('#niveau4').text("Niveau [5]  (" + fillState[4] + ")");
     if (fillState[4] == 0) {
         $('#niveau4').addClass("disabled");
     }
