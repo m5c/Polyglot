16430b9fece062c4ddf3c3d88a6abd8c9b462d58
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2020-08-13 13:02:53 +0200

    fixed key-handler inconsistency on DB reset

--- src/main/resources/static/menu.js
+++ src/main/resources/static/menu.js
@@ -1,4 +1,11 @@
-async function registerKeys() {
+/**
+ * registers the keys for the main menu options, with excpetion to the 1-5 keys for the individual boxes, for they depend on the actual fillstate.
+ * @returns {Promise<void>}
+ */
+async function resetKeysAndUi() {
+
+    //clean slate - remove all keyevents / clickevents
+    $(document).unbind(); // <-- literally the only reliable way to get rid of a keyhandler.
 
     // register callback for "export" button
     $('#export').on('click', exportAllCards);
@@ -12,9 +19,6 @@ async function registerKeys() {
     });
 
     // register callback for "import" button
-    //$('#import').on('click', importCardsFromFile);
-    //$('#import-form').addEventListener('change', handleFileSelect, false);
-    //.submit(importCardsFromFile);
     document.getElementById('import-form').addEventListener('change', handleFileSelect, false);
 
 
@@ -39,45 +43,13 @@ async function registerKeys() {
         }
     });
 
-    // register key-code access to levels, if non empty
-    if (fillState[0] != 0) {
-        $(document).keyup(function (e) {
-            if (e.key === "1") {
-                window.location.href = "/polyglot/niveau.html?level=1";
-            }
-        });
-    }
-    if (fillState[1] != 0) {
-
-        $(document).keyup(function (e) {
-            if (e.key === "2") {
-                window.location.href = "/polyglot/niveau.html?level=2";
-            }
-        });
-    }
-    if (fillState[2] != 0) {
-        $(document).keyup(function (e) {
-            if (e.key === "3") {
-                window.location.href = "/polyglot/niveau.html?level=3";
-            }
-        });
-    }
-    if (fillState[3] != 0) {
-        $(document).keyup(function (e) {
-            if (e.key === "4") {
-                window.location.href = "/polyglot/niveau.html?level=4";
-            }
-        });
-    }
-    if (fillState[4] != 0) {
-        $(document).keyup(function (e) {
-            if (e.key === "5") {
-                window.location.href = "/polyglot/niveau.html?level=5";
-            }
-        });
-    }
+    await showFillState();
 }
 
+/**
+ * query fills tate per box from api, update enabled/disabled UI visualization, enable/disable click and key based access.
+ * @returns {Promise<void>}
+ */
 async function showFillState() {
 
     const fillState = await getData('/polyglot/api/');
@@ -85,22 +57,58 @@ async function showFillState() {
     $('#niveau0').text("Niveau [1]  (" + fillState[0] + ")");
     if (fillState[0] == 0) {
         $('#niveau0').addClass("disabled");
+    } else {
+        // enable that box
+        $('#niveau0').removeClass("disabled");
+        $(document).keyup(function (e) {
+            if (e.key === "1") {
+                window.location.href = "/polyglot/niveau.html?level=1";
+            }
+        });
     }
     $('#niveau1').text("Niveau [2]  (" + fillState[1] + ")");
     if (fillState[1] == 0) {
         $('#niveau1').addClass("disabled");
+    } else {
+        $('#niveau1').removeClass("disabled");
+        $(document).keyup(function (e) {
+            if (e.key === "2") {
+                window.location.href = "/polyglot/niveau.html?level=2";
+            }
+        });
     }
     $('#niveau2').text("Niveau [3]  (" + fillState[2] + ")");
     if (fillState[2] == 0) {
         $('#niveau2').addClass("disabled");
+    } else {
+        $('#niveau2').removeClass("disabled");
+        $(document).keyup(function (e) {
+            if (e.key === "3") {
+                window.location.href = "/polyglot/niveau.html?level=3";
+            }
+        });
     }
     $('#niveau3').text("Niveau [4]  (" + fillState[3] + ")");
     if (fillState[3] == 0) {
         $('#niveau3').addClass("disabled");
+    } else {
+        $('#niveau3').removeClass("disabled");
+        $(document).keyup(function (e) {
+            if (e.key === "4") {
+                window.location.href = "/polyglot/niveau.html?level=4";
+            }
+        });
     }
     $('#niveau4').text("Niveau [5]  (" + fillState[4] + ")");
     if (fillState[4] == 0) {
         $('#niveau4').addClass("disabled");
+    } else {
+        $('#niveau4').removeClass("disabled");
+        $(document).keyup(function (e) {
+            if (e.key === "5") {
+                window.location.href = "/polyglot/niveau.html?level=5";
+            }
+        });
     }
 }
 
@@ -114,7 +122,7 @@ async function getData(url) {
  * reads out content of selected file (import context menu.)
  * @param event
  */
-function handleFileSelect(event){
+function handleFileSelect(event) {
     const reader = new FileReader()
     reader.onload = handleFileLoad;
     reader.readAsText(event.target.files[0]);
@@ -124,22 +132,18 @@ function handleFileSelect(event){
  * extract content of uploaded file, send to backend.
  * @param event
  */
-async function handleFileLoad(event){
+async function handleFileLoad(event) {
     const content = event.target.result;
     const contentAsJsonArray = JSON.parse(content);
     await postCards(contentAsJsonArray);
 
     // finally update the fill-state of the boxes (has changed due to import)
-    await showFillState();
-
-    // do this twice with slide delay to overcome lazy DB actions, in case the server RT is faster than the DB operations. (usually only on localhost.)
-    await sleep(100);
-    await showFillState();
+    await sleep(300);
+    await resetKeysAndUi();
 
 }
 
-async function exportAllCards()
-{
+async function exportAllCards() {
     let allCardsJson = await getData('api/cards/');
 
     // remove the database card id entry fpr every card in array.
@@ -177,35 +181,34 @@ function download(filename, text) {
  * @returns {string}
  */
 function getDateTimeString() {
-    var now     = new Date();
-    var year    = now.getFullYear();
-    var month   = now.getMonth()+1;
-    var day     = now.getDate();
-    var hour    = now.getHours();
-    var minute  = now.getMinutes();
-    var second  = now.getSeconds();
-    if(month.toString().length == 1) {
-        month = '0'+month;
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = now.getMonth() + 1;
+    var day = now.getDate();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+    if (month.toString().length == 1) {
+        month = '0' + month;
     }
-    if(day.toString().length == 1) {
-        day = '0'+day;
+    if (day.toString().length == 1) {
+        day = '0' + day;
     }
-    if(hour.toString().length == 1) {
-        hour = '0'+hour;
+    if (hour.toString().length == 1) {
+        hour = '0' + hour;
     }
-    if(minute.toString().length == 1) {
-        minute = '0'+minute;
+    if (minute.toString().length == 1) {
+        minute = '0' + minute;
     }
-    if(second.toString().length == 1) {
-        second = '0'+second;
+    if (second.toString().length == 1) {
+        second = '0' + second;
     }
-    let dateTime = year+'-'+month+'-'+day+'--'+hour+'-'+minute+'-'+second;
+    let dateTime = year + '-' + month + '-' + day + '--' + hour + '-' + minute + '-' + second;
     return dateTime;
 }
 
 // hidden developer option. D key resets the DB.
-async function resetDatabase()
-{
+async function resetDatabase() {
     const init = {
         method: 'DELETE'
     };
@@ -221,8 +224,8 @@ async function resetDatabase()
             // error in e.message
         });
 
-    await sleep(100);
+    await sleep(300);
 
     // update view (fill meter)
-    await showFillState();
+    await resetKeysAndUi();
 }
